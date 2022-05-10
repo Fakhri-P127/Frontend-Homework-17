fb3e840426ea79829e01ebf4ba6ce51bf4dcfcb9
Fakhri-P127 <fakhrita@code.edu.az>
2022-05-10 23:22:30 +0400

    added some changes to Task-Adidas folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
 const header = document.querySelector("header");
+const categories = document.querySelector(".third-header  #categories");
+const btnHamburger = document.querySelector("#btnHamburger");
+const btnClose = document.querySelector("#btnClose");
+
+btnHamburger.addEventListener("click", () => {
+  categories.classList.add("hamburger-showup");
+  categories.style.display = "flex";
+  categories.style.transition = "1s all";
+  btnClose.classList.remove("hidden");
+  btnClose.style.zIndex = "1";
+  btnClose.classList.add("close-btn-place");
+});
+
+btnClose.addEventListener("click", () => {
+  categories.classList.remove("hamburger-showup");
+  categories.style.display = "none";
+  btnClose.classList.add("hidden");
+  btnClose.classList.remove("close-btn-place");
+});
 
 let latestScroll = 0;
 window.addEventListener("scroll", () => {
@@ -11,4 +30,20 @@ window.addEventListener("scroll", () => {
     header.style.transition = "0.3s all";
   }
   latestScroll = scroll;
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 70,
+  responsive: {
+    0: {
+      items: 1.5,
+    },
+    600: {
+      items: 2.5,
+    },
+    1000: {
+      items: 4.5,
+    },
+  },
 });
